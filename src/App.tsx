347140cd3823d17a5cc7a28b929ef975a3ac2380
david-gilmar-martinello <80522558+david-gilmar-martinello@users.noreply.github.com>
2023-01-30 14:01:20 -0400

    feat - ajuste home

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-
+import logoDev from "../src/assets/logoDgProducoes.png";
 function App() {
-
   return (
-    <div className="flex justify-center items-center w-screen h-screen ">
-      <div className="">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="flex justify-center items-center w-screen h-screen bg-purple-600 ">
+      <div className="flex flex-col justify-center items-center gap-4 bg-white p-4  rounded-lg shadow-2xl">
+        <h1 className="text-3xl font-bold text-purple-600 pb-10 ">
+          Template - TailwindCSS!
+        </h1>
+        <img
+          className="flex rounded-full h-52 animate-bounce shadow-2xl border border-purple-600"
+          src={logoDev}
+          alt=""
+        />
       </div>
-      
     </div>
   );
 }
